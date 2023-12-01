523f76c22a6b2ce6a1478bc35fc7907ea6d9b48c
Tran <ttran1861@student.cccd.edu>
2023-11-30 19:11:41 -0800

    Updated both files locally

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ This is a demo for **CS220**.
 ### Section 1.1
 This is a change made locally!
 ### Section 1.2
+Another local change.
 ## Section 2
--- demo220.cpp
+++ demo220.cpp
@@ -3,5 +3,7 @@
 int main()
 {
   cout << "hello world." << endl;
+  cout << "This is changed locally." << endl;
+  cout << "Another local change." << endl;
   return 0;
 }
